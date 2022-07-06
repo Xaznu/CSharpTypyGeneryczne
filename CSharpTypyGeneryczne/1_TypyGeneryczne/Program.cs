8e66695d3169718527c9aebf737d6589551c4205
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-07-06 22:55:58 +0200

    pierwsza kolejka utworzona

--- CSharpTypyGeneryczne/1_TypyGeneryczne/Program.cs
+++ CSharpTypyGeneryczne/1_TypyGeneryczne/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace _1_TypyGeneryczne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var kolejka = new KolejkaKolowa(pojemnosc: 3);
+
+            while (true)
+            {
+                var wartosc = 0.0;
+                var wartoscWejsciowa = Console.ReadLine();
+
+                if (double.TryParse(wartoscWejsciowa, out wartosc))
+                {
+                    kolejka.Zapisz(wartosc);
+                    continue;
+                }
+                break;
+            }
+
+            Console.WriteLine("W naszej kolejce jest :");
+
+            while (!kolejka.JestPusty)
+            {
+                Console.WriteLine("\t\t" + kolejka.Czytaj());
+            }
+        }
+    }
+}
