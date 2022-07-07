122a531df265f6e4544e09b1e9ed17c506caaa39
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-07-07 19:13:51 +0200

    listy, dwukrotności

--- CSharpTypyGeneryczne/2_KolekcjeGeneryczne/Program.cs
+++ CSharpTypyGeneryczne/2_KolekcjeGeneryczne/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace _2_KolekcjeGeneryczne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var liczby = new List<int>();
+            var pojemnosc = -1;
+
+            while (true)
+            {
+                if (liczby.Capacity != pojemnosc)
+                {
+                    pojemnosc = liczby.Capacity;
+                    Console.WriteLine(pojemnosc);
+                }
+
+                liczby.Add(1);
+            }
+
+        }
+    }
+}
