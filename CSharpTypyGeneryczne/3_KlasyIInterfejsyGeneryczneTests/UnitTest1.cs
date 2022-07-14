feda6ffa097563e5bf2d51b9669d7c29e0343ed1
Kewin 131907 <chx94137@student.chorzow.wsb.pl>
2022-07-14 20:19:06 +0200

    iKolejka na typach generycznych

---
+++ CSharpTypyGeneryczne/3_KlasyIInterfejsyGeneryczneTests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace _3_KlasyIInterfejsyGeneryczneTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
